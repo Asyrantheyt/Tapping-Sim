e2b921f03d54e25c39e4b8880cd4233906b51936
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2023-04-08 10:01:54 +0800

    Update Scriptdeof.lua

--- Scriptdeof.lua
+++ Scriptdeof.lua
@@ -1,6 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "Tapping Sim", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTappingSim"})
+local camera = game.workspace.CurrentCamera
 
 _G.AutoClick = false
 _G.AutoRebirth = false
@@ -17,7 +18,13 @@ function AutoClick()
 		}
 
 		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Tap"):FireServer(unpack(args))
-		wait(0.000000000000001)
+
+		local args = {
+    		[1] = "Events"
+		}
+
+		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Tap"):FireServer(unpack(args))
+		wait()
 	end
 end
 
@@ -29,21 +36,69 @@ function AutoRebirth()
 
 		game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Rebirth"):FireServer(unpack(args))
 
-		wait(0.0000000000001)
+		wait()
 	end
 end
 
 function PlayerEsps()
 while _G.PlayerEsp == true do
-	for _,v in pairs(game.Workspace:GetChildren()) do
-		if v.Name == Players:GetChildren().Name then
-				local esp = Instance.new("Highlight", v)
-				esp.FillTransparency = 1
-				esp.OutlineColor = 0,255,0
-			end
-		end	
-	end
 	wait()
+	for _,v in pairs(game.Players:GetPlayers()) do
+			if v.Character ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+				local vector,OnScreen = camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+				local Line = Drawing.new("Line")
+				Line.Visible = true
+				Line.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
+				Line.To = Vector2.new(vector.X, vector.Y)
+				Line.Color = Color3.fromRGB(255, 80, 60)
+				Line.Thickness = 2
+				Line.Transparency = 1
+
+				function esplinescript()
+				game:GetService("RunService").RenderStepped:Connect(function(step)
+						if v.Character ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+							local vector,OnScreen = camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+							if OnScreen == true then
+								Line.To = Vector2.new(vector.X,vector.Y)
+								Line.Visible = true
+								Line.Visible = _G.PlayerEsp
+							else
+								Line.Visible = false
+							end
+						end
+					end)
+				end
+			end
+			esplinescript()
+		end
+		v.CharacterAdded:Connect(function()
+			repeat wait() until v.Character ~= nil
+			repeat wait() until v.Character:FindFirstCild("HumanoidRootPart")
+			local vector,OnScreen = camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+			local Line = Drawing.new("Line")
+			Line.Visible = true
+			Line.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
+			Line.To = Vector2.new(200, 200)
+			Line.Color = Color3.fromRGB(255, 80, 60)
+			Line.Thickness = 2
+			Line.Transparency = 1
+
+			function esplinescript2()
+				game:GetService("RunService").RenderStepped:Connect(function(step)
+					if v.Character ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+						local vector,OnScreen = camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+						if OnScreen == true then
+							Line.To = Vector2.new(vector.X,vector.Y)
+							Line.Visible = true
+							Line.Visible = _G.PlayerEsp
+						else
+							Line.Visible = false
+						end
+					end
+				end)
+			end
+		end)
+	end
 end
 
 local AutoFarm = Window:MakeTab({
@@ -85,13 +140,17 @@ local Esp = Window:MakeTab({
 	PremiumOnly = false
 })
 
+Esp:AddLabel("this might not work due to")
+Esp:AddLabel("'obfuscate'")
+
+	
 Esp:AddToggle({
 	Name = "Player Esp (WILL CRASH YOU GAME)",
 	Default = false,
 	Callback = function(Value)
-	_G.PlayerEsp = Value
+		_G.PlayerEsp = Value
 		PlayerEsps()
-	end   
+	end
 })
 
 OrionLib:Init()
